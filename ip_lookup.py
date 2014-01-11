69c00d174c4e30e084db84f2a45608784d97a7d4
Andrew Kelly <kellyaj@gmail.com>
2014-01-11 17:22:15 -0600

    update lookup

--- ip_lookup.py
+++ ip_lookup.py
@@ -2,14 +2,35 @@ import re
 import subprocess
 import json
 
-lastLoginCommand = "cat /var/log/auth.log | grep 'sshd.*Accepted' | tail -1"
-rawLoginString = subprocess.check_output(lastLoginCommand, shell=True)
-ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', rawLoginString)[0] 
+class Governor(object):
 
-if ip[0:7] == "192.168":
-    print "Chicago"
-else:
-    lookupString = "curl -s http://dazzlepod.com/ip/" + ip + ".json"
-    lookupResult = subprocess.check_output(lookupString, shell=True)
-    jsonResult = json.loads(lookupResult)
-    print jsonResult["city"]
+    @staticmethod
+    def run_command(command_string):
+        return subprocess.check_output(command_string, shell=True)
+
+    @staticmethod
+    def parse_ip(raw_ip_string):
+        return re.findall( r'[0-9]+(?:\.[0-9]+){3}', raw_ip_string)[0]
+
+    @staticmethod
+    def lookup(ip):
+        if ip[0:7] == "192.168":
+            return "Home"
+        else:
+            lookup_string = "curl -s http://dazzlepod.com/ip/" + ip + ".json"
+            lookup_result = run_command(lookup_string)
+            json_result = json.loads(lookup_result)
+            return json_result["city"]
+
+login_1 = "cat /var/log/auth.log | grep 'sshd.*Accepted' | tail -2 | head -1"
+login_2 = "cat /var/log/auth.log | grep 'sshd.*Accepted' | tail -3 | head -1"
+login_3 = "cat /var/log/auth.log | grep 'sshd.*Accepted' | tail -4 | head -1"
+
+commands = [login_1, login_2, login_3]
+
+things = []
+
+for idx, command in enumerate(commands):
+    ip = Governor.parse_ip(Governor.run_command(command))
+    things.append(Governor.lookup(ip))
+print ", ".join(things)
